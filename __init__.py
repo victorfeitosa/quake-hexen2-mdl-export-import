168f0d5d2d201bd23de862925f40ceb66d826fd6
Victor Feitosa <victor.klngo@gmail.com>
2020-08-25 20:37:15 -0400

    Upped version

--- __init__.py
+++ __init__.py
@@ -23,7 +23,7 @@ bl_info = {
     "name": "Quake and Hexen II MDL format",
     "author": "Bill Currie, Victor Feitosa",
     "blender": (2, 80, 0),
-    "version": (0, 8, 0),
+    "version": (0, 9, 0),
     "api": 35622,
     "location": "File > Import-Export",
     "description": "Import-Export Quake and HexenII MDL files (version 6 mdl files)",
