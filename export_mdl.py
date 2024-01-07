Bugfix/fix scaling factor (#13)
* fix import scale

* adjust scale_factor usage

* cleanup and remove comments

export_mdl.py
@@ -125,7 +125,8 @@ def make_skin(mdl, mesh, palette):
                     if node.type == "TEX_IMAGE":
                         image = node.image
                         if (image.size[0] > 0 and image.size[1] > 0):
-                            mdl.skinwidth, mdl.skinheight = (image.size[0], image.size[1])
+                            mdl.skinwidth, mdl.skinheight = (
+                                image.size[0], image.size[1])
                             skin = convert_image(image, palette)
                         mdl.skins.append(skin)
             else:
@@ -210,7 +211,7 @@ def scale_verts(mdl):
         tf.add_frame(f, 0.0)    # let the frame class do the dirty work for us
     size = Vector(tf.maxs) - Vector(tf.mins)
     rsqr = tuple(map(lambda a, b: max(abs(a), abs(b)) ** 2, tf.mins, tf.maxs))
-    mdl.boundingradius = (rsqr[0] + rsqr[1] + rsqr[2]) ** 0.5
+    mdl.boundingradius = ((rsqr[0] + rsqr[1] + rsqr[2]) ** 0.5)
     mdl.scale_origin = tf.mins
     mdl.scale = tuple(map(lambda x: x / 255.0, size))
     for f in mdl.frames:
@@ -325,14 +326,9 @@ def export_mdl(operator, context, filepath, palette, export_scale):
     depsgraph = context.evaluated_depsgraph_get()
     ob_eval = obj.evaluated_get(depsgraph)
     objname = ob_eval.name_full
-    bpy.ops.transform.resize(value=(export_scale, export_scale, export_scale))
 
     palette = getPaletteFromName(palette)
 
-    # if not check_faces(mesh):
-    #    operator.report({'ERROR'},
-    #                    "Mesh has faces with more than 3 vertices.")
-    #    return {'CANCELLED'}
     mdl = MDL(obj.name)
     mdl.obj = obj
     if not get_properties(operator, mdl, obj, export_scale):
@@ -358,10 +354,9 @@ def export_mdl(operator, context, filepath, palette, export_scale):
 
     mdl.numverts = len(mdl.frames[0].verts)
     convert_stverts(mdl, mdl.stverts)
+    mdl.scale_factor = export_scale
     mdl.size = calc_average_area(mdl)
     scale_verts(mdl)
     mdl.write(filepath)
-    bpy.ops.transform.resize(
-        value=(1/export_scale, 1/export_scale, 1/export_scale))
 
     return {'FINISHED'}